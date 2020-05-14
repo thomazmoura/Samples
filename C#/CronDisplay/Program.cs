08bb8fbc400c8895547ca032b7e154f7147216a8
Thomaz Moura <thomaz.moura@autotrac.com.br>
2020-05-13 22:44:15 -0300

    Add cron sample with the last day of the month

---
+++ C#/CronDisplay/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cronos;
+
+namespace CronDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!args.Any())
+            {
+                Console.WriteLine("You need to inform the Cron Schedule");
+                return;
+            };
+            var cronSchedule = args[0];
+
+            var cronExpression = CronExpression.Parse(cronSchedule, CronFormat.IncludeSeconds);
+            var next20OcurrencesThisYear = cronExpression
+                .GetOccurrences(DateTime.UtcNow, DateTime.UtcNow.AddYears(1))
+                .Take(20);
+
+            foreach(var ocurrence in next20OcurrencesThisYear)
+            {
+                if(ocurrence == next20OcurrencesThisYear.First())
+                    Console.WriteLine($"First it will fire on {ocurrence}, which is on {ocurrence.ToLocalTime()} on local time");
+                else
+                    Console.WriteLine($"Then it will fire on {ocurrence}, which is on {ocurrence.ToLocalTime()} on local time");
+            }
+        }
+    }
+}
