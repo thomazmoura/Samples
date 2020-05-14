198f7ae0fd537e4915f5b5bf6011274442222786
Thomaz Moura <thomaz.moura@autotrac.com.br>
2020-05-13 22:16:47 -0300

    Add a client credentials connection sample

---
+++ C#/ClientCredentialsConnecter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace ClientCredentialsConnecter
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string clientId, clientSecret, scope;
+            if(args.Length >= 3)
+            {
+                clientId = args[0];
+                clientSecret = args[1];
+                scope = args[2];
+            }
+            else
+            {
+                Console.WriteLine("It's necessary to inform both client ID, client secret and scope");
+                return;
+            }
+
+            var authHttpClient = new HttpClient();
+            var discoveryEndpoint = await authHttpClient.GetDiscoveryDocumentAsync("https://www1.autotrac.com.br/ids");
+            var tokenResponse = await authHttpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                Address = discoveryEndpoint.TokenEndpoint,
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+                Scope = scope
+            });
+
+            if(tokenResponse.IsError)
+                Console.WriteLine($"It wasn't possible to connect with the authority. Detalhes: {tokenResponse.Error}");
+            else
+                Console.WriteLine(tokenResponse.AccessToken);
+        }
+    }
+}
