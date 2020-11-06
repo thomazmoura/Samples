119531b3fa889368a4a45b77436827e5fe4de720
Thomaz Moura <thomaz.moura@autotrac.com.br>
2020-11-06 14:57:18 -0300

    Added sample of error-inducing false implicit initialization

---
+++ C#/ImplicitInstantiation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ImplicitInstantiation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inicializacaoQueFunciona = new ClassePai
+            {
+                Propriedade = new ClasseDaPropriedade
+                {
+                    ValorDoTeste = "FirulaDefinida"
+                }
+
+            };
+            ClassePai inicializacaoQueCagaTudo = new ClassePai
+            {
+                Propriedade = {  }
+            };
+
+            Console.WriteLine(inicializacaoQueFunciona.Propriedade.ValorDoTeste);
+            Console.WriteLine(inicializacaoQueCagaTudo.Propriedade.ValorDoTeste);
+        }
+    }
+
+    class ClassePai
+    {
+        public ClasseDaPropriedade Propriedade { get; set; }
+    }
+
+    class ClasseDaPropriedade
+    {
+        public ClasseDaPropriedade()
+        {
+            ValorDoTeste = "Firula";
+        }
+        public string ValorDoTeste { get; set; }
+    }
+}
