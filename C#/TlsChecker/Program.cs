d96467e46fbdebde24ba9ce05cf325ba97edde41
Thomaz Moura <thomaz.moura@autotrac.com.br>
2020-11-06 14:38:45 -0300

    Add code to check current TLS settings

---
+++ C#/TlsChecker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Security;
+
+namespace TlsChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ServicePointManager.SecurityProtocol);
+            
+
+        }
+    }
+}
