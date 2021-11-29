a9f6febf294eac08a6cb2c524cf26a3877275864
Thomaz Moura <thomaz.moura@autotrac.com.br>
2021-11-29 10:26:08 -0300

    Record sample

---
+++ C#/RecordFunctions/Program.cs
@@ -0,0 +1,15 @@
+﻿var currency = "Bolsos";
+var firstMoney = new Money(currency, 10);
+var secondMoney = new Money(currency, 10);
+var thirdMoney = firstMoney.Add(secondMoney);
+Console.WriteLine($"Adding {firstMoney.Amount} {firstMoney.Currency} to {secondMoney.Amount} {secondMoney.Currency} results in {thirdMoney.Amount} {thirdMoney.Currency}");
+
+public record Money(string Currency, int Amount)
+{
+    public Money Add(Money other) {
+        if(Currency == other.Currency)
+            return new Money(Currency, Amount + other.Amount);
+        throw new ArgumentException($"Cannot add {other} to this");
+    }
+}
+
