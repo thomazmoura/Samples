d1d2049b74599668e1c2909436b01c62fdfcf04f
Thomaz Moura <thomaz.moura@autotrac.com.br>
2022-01-06 23:43:02 -0300

    Update to .NET 6

--- C#/CronDisplay/Program.cs
+++ C#/CronDisplay/Program.cs
@@ -1,33 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Cronos;
+﻿using Cronos;
 
-namespace CronDisplay
+if(!args.Any())
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if(!args.Any())
-            {
-                Console.WriteLine("You need to inform the Cron Schedule");
-                return;
-            };
-            var cronSchedule = args[0];
+    Console.WriteLine("You need to inform the Cron Schedule");
+    return;
+};
 
-            var cronExpression = CronExpression.Parse(cronSchedule, CronFormat.IncludeSeconds);
-            var next20OcurrencesThisYear = cronExpression
-                .GetOccurrences(DateTime.UtcNow, DateTime.UtcNow.AddYears(1))
-                .Take(20);
+var cronSchedule = args[0];
+var cronExpression = CronExpression.Parse(cronSchedule, CronFormat.IncludeSeconds);
+var next20OcurrencesThisYear = cronExpression
+    .GetOccurrences(DateTime.UtcNow, DateTime.UtcNow.AddYears(1))
+    .Take(20);
+var firstOcurrence = next20OcurrencesThisYear.First();
 
-            foreach(var ocurrence in next20OcurrencesThisYear)
-            {
-                if(ocurrence == next20OcurrencesThisYear.First())
-                    Console.WriteLine($"First it will fire on {ocurrence}, which is on {ocurrence.ToLocalTime()} on local time");
-                else
-                    Console.WriteLine($"Then it will fire on {ocurrence}, which is on {ocurrence.ToLocalTime()} on local time");
-            }
-        }
-    }
-}
+Console.WriteLine($"First it will fire on {firstOcurrence}, which is on {firstOcurrence.ToLocalTime()} on local time");
+foreach(var ocurrence in next20OcurrencesThisYear.Skip(1))
+    Console.WriteLine($"Then it will fire on {ocurrence}, which is on {ocurrence.ToLocalTime()} on local time");
