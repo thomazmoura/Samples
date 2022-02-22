e030097d742b4b3a6adede2436c310fb6121e5a3
Thomaz Moura <padilha.thomaz@gmail.com>
2022-02-22 11:47:36 -0300

    Add samples of .NET 6 context with Nullable References

---
+++ C#/DotNet6Context/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+public class SampleContext: DbContext
+{
+    public DbSet<SampleClass> SampleClass => Set<SampleClass>();
+}
+
+public class SampleClass
+{
+    public string Name { get; set; } = "Unnamed";
+    public string Description { get; set; } = "No description.";
+}
