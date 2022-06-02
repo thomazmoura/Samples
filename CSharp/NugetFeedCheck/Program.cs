b9a4e350edf04ff06ae26265061523ee0fe21ad4
Thomaz Moura <padilha.thomaz@gmail.com>
2022-06-02 12:06:34 +0000

    Add NugetFeedCheck to CSharp samples

---
+++ CSharp/NugetFeedCheck/Program.cs
@@ -0,0 +1,12 @@
+﻿
+var uriOnArgs = args
+    .Where(arg => arg.StartsWith("https://"));
+var uri = uriOnArgs.Any() ?
+    uriOnArgs.First() :
+    "https://api.nuget.org/v3/index.json";
+
+var client = new HttpClient();
+Console.WriteLine($"URI to be called: {uri}");
+var response = await client.GetAsync(uri);
+string msg = $"If you see this, your machine has no TLS/SSL issues calling {uri}";
+Console.WriteLine(msg);
