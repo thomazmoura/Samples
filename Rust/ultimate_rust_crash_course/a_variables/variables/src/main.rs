21b7795e84de3985db683d5fc1526b96ec595416
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-28 01:25:44 -0300

    Add a little bit of flair to the first exercise

--- Rust/ultimate_rust_crash_course/a_variables/variables/src/main.rs
+++ Rust/ultimate_rust_crash_course/a_variables/variables/src/main.rs
@@ -2,14 +2,24 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    fire_missiles();
+    let mut remaining_missiles = STARTING_MISSILES;
+    for i in 0..5 {
+        println!("Running for the {i:?}th time");
+        remaining_missiles = fire_missiles(remaining_missiles);
+    }
 }
 
-fn fire_missiles() {
-    let (mut missiles, ready):(i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+fn fire_missiles(current_missiles: i32) -> i32 {
+    if current_missiles <= 0 {
+        println!("No missiles left to fire!");
+        return 0;
+    }
+
+    let (mut missiles, ready):(i32, i32) = (current_missiles, READY_AMOUNT);
     println!("Firing {ready} of my {missiles} missiles...");
-    missiles = calculate_remaining_missiles(missiles);
+    missiles = calculate_remaining_missiles(missiles, ready);
     println!("Only {missiles} missiles left.");
+    return missiles;
 }
 
-fn calculate_remaining_missiles(current_missiles: i32) -> i32 { current_missiles - READY_AMOUNT }
+fn calculate_remaining_missiles(current_missiles: i32, ready_amount: i32) -> i32 { current_missiles - ready_amount }
