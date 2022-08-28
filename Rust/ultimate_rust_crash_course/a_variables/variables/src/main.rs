29dee418840baf9a43e8b5df40c86249a28586a7
Thomaz Moura <padilha.thomaz@gmail.com>
2022-08-28 01:13:58 -0300

    Add function calls

--- Rust/ultimate_rust_crash_course/a_variables/variables/src/main.rs
+++ Rust/ultimate_rust_crash_course/a_variables/variables/src/main.rs
@@ -2,8 +2,14 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
+    fire_missiles();
+}
+
+fn fire_missiles() {
     let (mut missiles, ready):(i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {ready} of my {missiles} missiles...");
-    missiles -= ready;
+    missiles = calculate_remaining_missiles(missiles);
     println!("Only {missiles} missiles left.");
 }
+
+fn calculate_remaining_missiles(current_missiles: i32) -> i32 { current_missiles - READY_AMOUNT }
