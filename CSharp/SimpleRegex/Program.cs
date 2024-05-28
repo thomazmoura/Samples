c99b815a86fe3e584c86c0d0c98a4f0a1801b94f
Thomaz Moura <padilha.thomaz@gmail.com>
2024-05-28 16:34:51 -0300

    Add simple regex sample

---
+++ CSharp/SimpleRegex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SimpleRegex;
+
+public class Program
+{
+    public static string KeepOnlyNumeric(string input)
+    {
+        var numericOnlyPattern = @"\D";
+        return Regex.Replace(input, numericOnlyPattern, "");
+    }
+}
+
+public class ProgramTests
+{
+    [Theory]
+    [InlineData("000.000.000-00", "00000000000")]
+    public void KeepOnlyNumeric_ShouldReturnJustTheNumericCharacters(string input, string expectedOutput)
+    {
+        var result = Program.KeepOnlyNumeric(input);
+        result.Should().Be(expectedOutput);
+    }
+}
